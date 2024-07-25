8ff2587e0059d62a4246c9ca6cae09e88676643c
YedhuKrishna01 <yedhukrishnagk@gmail.com>
2024-07-25 22:47:10 +0530

    Carasole finishing touches

---
+++ Client/src/components/Works3D.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
+
+function Works3D() {
+  const slides = [
+    { url: "./upscale4.webp" },
+    { url: "./upscale2.webp" },
+    { url: "./upscale1.webp" },
+    { url: "./upscale3.webp" },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // Function to slide to the left
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  // Function to slide to the right
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  return (
+    <div>
+      <div className="text-white text-lg font-audio">3D-Models</div>
+      <div className="w-full h-56 py-4 px-1 relative">
+        <img
+          classname="w-full h-52 rounded-2xl duration-500"
+          src={`${slides[currentIndex].url}`}
+        />
+        <BsChevronCompactLeft
+          onClick={prevSlide}
+          size={30}
+          className="absolute top-[55%] left-5 text-2xl rounded-full p-2 text-gray-400 cursor-pointer"
+        />
+        <BsChevronCompactRight
+          onClick={nextSlide}
+          size={30}
+          className="absolute top-[55%] right-5 text-2xl rounded-full p-2 text-gray-400 cursor-pointer"
+        />
+      </div>
+      <div className="flex top-4 justify-center py-2 mt-5">
+        {slides.map((_, slideIndex) => (
+          <RxDotFilled
+            key={slideIndex}
+            className={`border-2 scale-50 rounded-full border-white cursor-pointer ${
+              currentIndex === slideIndex ? "text-white" : "text-black"
+            }`}
+            onClick={() => setCurrentIndex(slideIndex)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Works3D;
--- Client/src/pages/Works.jsx
+++ Client/src/pages/Works.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import WorksBody from "../components/WorksBody";
+import Works3D from "../components/Works3D";
+import WorksDesigns from "../components/WorksDesigns";
 
 function Works() {
   return (
     <div>
       <Navbar />
-      <WorksBody />
+      <WorksDesigns />
+      <Works3D />
     </div>
   );
 }
