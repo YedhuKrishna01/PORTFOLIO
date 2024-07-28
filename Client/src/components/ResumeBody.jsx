9f31de51534ed50210b57e429061b1bbe5e5b192
YedhuKrishna01 <yedhukrishnagk@gmail.com>
2024-07-28 22:54:29 +0530

    attached resume body

---
+++ Client/src/components/ResumeBody.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ResumeBody() {
+  return (
+    <div>
+      <div className="fhhr">vfdgdgh</div>
+    </div>
+  );
+}
+
+export default ResumeBody;
--- Client/src/pages/Resume.jsx
+++ Client/src/pages/Resume.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import ResumeBody from "../components/ResumeBody";
 
 function Resume() {
   return (
     <div>
       <Navbar />
+      <ResumeBody />
     </div>
   );
 }
