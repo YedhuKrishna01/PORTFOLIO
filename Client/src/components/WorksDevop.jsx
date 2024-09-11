3e777d20e312b2a829a890069f7fe773e9d8125c
YedhuKrishna01 <yedhukrishnagk@gmail.com>
2024-09-11 21:59:39 +0530

    working on some changes in the work page

--- Client/src/components/WorksDevop.jsx
+++ Client/src/components/WorksDevop.jsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import { RxDotFilled } from "react-icons/rx";
-import { Link } from "react-router-dom";
 
 function WorksDevop() {
   const slides = [
-    { url: "./dev1.jpeg", site: "https://yedhukrishnagk.netlify.app" },
-    { url: "./dev2.png", site: "https://resena.netlify.app" },
+    {
+      url: "./dev1.jpeg",
+      site: "https://yedhukrishnagk.netlify.app",
+      name: "Portfolio Site",
+    },
+    {
+      url: "./dev2.png",
+      site: "https://resena.netlify.app",
+      name: "Movie Review Site",
+    },
   ];
+
   const [currentIndex, setCurrentIndex] = useState(0);
 
   // Function to slide to the left
@@ -26,17 +34,24 @@ function WorksDevop() {
 
   return (
     <div className="grid grid-cols-12 mt-7">
-      <div className="text-white col-start-2 col-span-10 text-lg tablet:text-3xl laptop:text-4xl tablet:pl-4 font-audio">
-        Development Projects
+      <div className="text-white col-start-2 col-span-10 text-lg tablet:text-3xl tablet:pl-4 laptop:text-4xl font-audio">
+        Developed Projects
       </div>
-      <div className="col-start-2 col-span-10 w-full h-auto tablet:mt-3 rounded-lg desktop:mt-5 py-4 px-1 relative">
-        <a href={slides[currentIndex].site} target="_blank">
-          <img
-            classname="w-full h-40 tablet:h-80 laptop:h-[500px] desktop:h-[660px] slide rounded-lg laptop:rounded-2xl duration-500"
-            src={slides[currentIndex].url}
-          />
-        </a>
-
+      <div className="bg-zinc-200 col-span-12 tablet:mt-3 h-auto py-4 px-8 relative">
+        <img
+          className="w-full h-40 tablet:h-80 laptop:h-[500px] desktop:h-[660px] slide rounded-lg laptop:rounded-2xl duration-500"
+          src={`${slides[currentIndex].url}`}
+          alt={slides[currentIndex].name}
+        />
+        <div className="text-center text-red border-2 border-red mt-4 p-1 rounded-md">
+          <a
+            href={slides[currentIndex].site}
+            target="_blank"
+            className="font-audio underline underline-offset-2"
+          >
+            {slides[currentIndex].name}
+          </a>
+        </div>
         <BsChevronCompactLeft
           onClick={prevSlide}
           size={30}
@@ -48,7 +63,7 @@ function WorksDevop() {
           className="absolute top-[45%] right-5 text-2xl tablet:scale-150 rounded-full p-2 text-gray-400 cursor-pointer"
         />
       </div>
-      <div className="col-start-2 col-span-10 flex m-5 justify-center">
+      <div className="col-start-2 col-span-10 flex mt-[-0.5rem] mb-5 justify-center">
         {slides.map((_, slideIndex) => (
           <RxDotFilled
             key={slideIndex}
